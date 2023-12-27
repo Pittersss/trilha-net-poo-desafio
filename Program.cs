4e4191eb6bfd8a6693c2ba6a6539dadf5be96370
Pedro Malaquias <pedromalaquias608@gmail.com>
2023-12-27 10:36:45 -0300

    Tudo feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("Iphone 12","Pro Max","3DRTF",128);
+iphone.InstalarAplicativo("Facebook");
+iphone.ReceberLigacao();
+iphone.Ligar();
+
+Nokia nokia = new Nokia("Nokia","Tijolão","4UYH3",10);
+iphone.InstalarAplicativo("MSN");
+iphone.ReceberLigacao();
+iphone.Ligar();
